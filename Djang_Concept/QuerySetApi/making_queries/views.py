28b075fddd1fb14bb97a9fb9cf2ba67b5942805d
DhaniswarBK <bkdhanishowor@gmail.com>
2023-06-07 22:11:52 +0545

    feat: created-serializers-and-views-fo-all-models

--- Djang_Concept/QuerySetApi/making_queries/views.py
+++ Djang_Concept/QuerySetApi/making_queries/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Author, Blog, Entry
+from .serializers import AuthorSerializer, BlogSerializer, EntrySerializer
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+
+class AuthorAPIViewDetails(APIView):
+    def get(self, request):
+        queryset = Author.objects.exclude(name="Paul")
+        serializer = AuthorSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    
+
+class BlogAPIViewDetails(APIView):
+    
+    def get(self, request):
+        queryset = Blog.objects.all()
+        serializer = BlogSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class EntryAPIViewDetails(APIView):
+    
+    def get(self, request):
+        queryset = Entry.objects.all()
+        serializer = EntrySerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
